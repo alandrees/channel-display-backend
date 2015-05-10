2ff414301944bc4e7fe312fc22fd60c7da932cfe
loridcon <loridcon@cake.lan>
2015-05-09 23:36:29 -0400

    added implementation for the MidiInput constructor which takes no arguments

--- src/midi_input.cpp
+++ src/midi_input.cpp
@@ -74,3 +74,36 @@ MidiInput::MidiInput(int port){
   this->midiin->setCallback(this->fireMessageEvent, this);
   this->midiin->ignoreTypes(false, false, false);
 }
+
+/**\fn MidiInput::MidiInput
+ *
+ * Overloaded MidiInput class constructor which creates an internal-only RtMidiIn object and defaults to device 0
+ *
+ * @param None
+ *
+ * @returns None
+ */
+
+MidiInput::MidiInput(){
+  try{
+    this->midiin = new RtMidiIn();
+  }
+  catch ( RtMidiError &error){
+    error.printMessage();
+    exit( EXIT_FAILURE );
+  }
+
+  this->external_midiin = false;
+
+  this->midi_instance = 0;
+
+  if(midiin->getPortCount() <= 0){
+    std::cout << "Insufficent MIDI Ports.  Exiting.\n";
+    exit( EXIT_FAILURE );
+  }
+
+  this->midiin->openPort(this->midi_instance);
+
+  this->midiin->setCallback(this->fireMessageEvent, this);
+  this->midiin->ignoreTypes(false, false, false);
+}
