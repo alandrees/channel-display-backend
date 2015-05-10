0ba023d4c4beba0c12adb7a5ffcf254439fb94dd
loridcon <loridcon@cake.lan>
2015-05-09 23:34:06 -0400

    added the MidiInput overloaded constructor taking the RtMidiIn object and port

--- src/midi_input.cpp
+++ src/midi_input.cpp
@@ -12,3 +12,31 @@
 #include "RtMidi.h"
 #include "input.hpp"
 #include "midi_input.hpp"
+
+/**\fn MidiInput::MidiInput
+ *
+ * MidiInput class constructor
+ *
+ * @param port (int) midi input port instance to open
+ * @param (RtMidiIn*) instance of RtMidiIn to pass to the
+ *
+ * @returns None
+ */
+
+MidiInput::MidiInput(int port, RtMidiIn* midiin){
+  this->midiin = midiin;
+
+  this->external_midiin = true;
+
+  this->midi_instance = port;
+
+  if(midiin->getPortCount() <= 0){
+    std::cout << "Insufficent MIDI Ports.  Exiting.\n";
+    exit( EXIT_FAILURE );
+  }
+
+  this->midiin->openPort(this->midi_instance);
+
+  this->midiin->setCallback(this->fireMessageEvent, this);
+  this->midiin->ignoreTypes(false, false, false);
+}
