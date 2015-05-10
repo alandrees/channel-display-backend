7284cc8feca1215ab573ba1ff7e103c8d1cebcbd
loridcon <loridcon@cake.lan>
2015-05-09 23:11:11 -0400

    added the CallbackMessage data structure

--- src/midi_input.hpp
+++ src/midi_input.hpp
@@ -17,4 +17,9 @@
 #define MIDI_INPUT_HP
 
 typedef void (*msg_callback)(std::vector<unsigned char>*); /**typedef for the message passing callback*/
+
+struct CallbackMessage {
+  msg_callback cb; /** callback function */
+  int id; /** identifier */
+};
 #endif
