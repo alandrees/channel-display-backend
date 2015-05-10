9e448c120e0f7d38094a3ed51b8edf645b200f37
loridcon <loridcon@cake.lan>
2015-05-09 23:32:35 -0400

    added include directives for midi_input.cpp

--- src/midi_input.cpp
+++ src/midi_input.cpp
@@ -4,3 +4,11 @@
  * Purpose:
  *   Implements the MidiInput class
  */
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <list>
+#include "RtMidi.h"
+#include "input.hpp"
+#include "midi_input.hpp"
