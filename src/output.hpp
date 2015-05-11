667f1167f68c7b6ac6e7bc31594fd6d5af725dd3
Alan Drees <alandrees@theselves.com>
2015-05-11 12:02:43 -0400

    added the class prototype for the Output base class

--- src/output.hpp
+++ src/output.hpp
@@ -14,4 +14,14 @@
 
 #ifndef OUTPUT_HPP
 #define OUTPUT_HPP
+class Output
+{
+private:
+  int instance; /**Instance of current object*/
+protected:
+  static int instances; /**Number of input instances*/
+public:
+  Output();
+  ~Output();
+};
 #endif
