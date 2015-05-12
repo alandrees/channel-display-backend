dbe06d4994ead74504c20e8b6134b31c7e469e85
Alan Drees <alandrees@theselves.com>
2015-05-12 06:15:01 -0400

    added basic class definition for the LCDOutput class

--- src/lcd_output.hpp
+++ src/lcd_output.hpp
@@ -17,4 +17,24 @@
 
 typedef std::vector<std::string> LineBuffer;
 typedef std::vector<LineBuffer*> VectorBuffer;
+
+class LCDOutput : Output{
+  VectorBuffer* line_buffers; /**current buffer filled by the updated input*/
+  VectorBuffer* current_output; /**current output buffer filled by flipping the line_buffer to the LCD output*/
+  int output_number; /**determines the LCD index to output to*/
+  int height; /**height of the device, in lines*/
+  int width; /**width of the device, in characters*/
+  bool outputToLCD(int, std::string);
+
+public:
+  LCDOutput(int, int, int);
+  ~LCDOutput();
+  void flushBuffer(int);
+  void flushAll();
+  void pushToBuffer(int, int, std::string);
+  void bulkUpdate(VectorBuffer, bool);
+  void clearBuffer(int);
+  void clearAll();
+  VectorBuffer getCurrentOutput();
+};
 #endif
