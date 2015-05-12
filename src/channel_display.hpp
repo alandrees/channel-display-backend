7a19ecf6fa1062fdf0f51c1190509bc7fa36544d
Alan Drees <alandrees@theselves.com>
2015-05-12 09:31:38 -0400

    added the include directives for the channel display header file

--- src/channel_display.hpp
+++ src/channel_display.hpp
@@ -4,3 +4,9 @@
  * Purpose:
  *  Header file for the main Channel Display application
  */
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include "midi_input.hpp"
+#include "lcd_output.hpp"
