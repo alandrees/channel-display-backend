0eb866d99be0ca80f89743f0305d1a9022343cdb
Alan Drees <alandrees@theselves.com>
2015-05-13 12:55:54 -0400

    added ChannelDisplay class constructor implementation

--- src/channel_display.cpp
+++ src/channel_display.cpp
@@ -13,3 +13,32 @@
 #include "lcd_output.hpp"
 #include "channel_display.hpp"
 #include "callback.hpp"
+
+/**\fn ChannelDisplay::ChannelDisplay
+ *
+ * ChannelDisplay class constructor
+ *
+ * @param None
+ *
+ * @returns None
+ */
+
+ChannelDisplay::ChannelDisplay(){
+  MidiInput* in = 0;
+  MidiCallback* mc = 0;
+
+  in = new MidiInput();
+
+  this->input.push_back(in);
+
+  LCDOutput *x = 0;
+
+  for(int i = 0; i < 8; i++){
+    x = new LCDOutput(20, 4, i);
+    this->output.push_back(x);
+  }
+
+  mc = this;
+
+  this->input[0]->addMessageCallback(&MidiCallback::midiCallback, mc);
+}
