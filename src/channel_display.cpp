124ba2a479562ee3e41ddeae91d983824b6ae466
Alan Drees <alandrees@theselves.com>
2015-05-13 12:55:22 -0400

    added include directives for main() and the ChannelDisplay class

--- src/channel_display.cpp
+++ src/channel_display.cpp
@@ -4,3 +4,12 @@
  * Purpose:
  *  Application entry point and implementation of the ChannelDisplay class
  */
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
+#include "midi_input.hpp"
+#include "lcd_output.hpp"
+#include "channel_display.hpp"
+#include "callback.hpp"
