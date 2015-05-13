5860e49d785b1acdea2b16db3b90ad7497d330be
Alan Drees <alandrees@theselves.com>
2015-05-13 12:58:07 -0400

    added main() function implementation

--- src/channel_display.cpp
+++ src/channel_display.cpp
@@ -87,3 +87,27 @@ void ChannelDisplay::midiCallback(midi_message message){
     std::cout << std::hex << (int)message.message.msgtype << " is not a command.\n";
   }
 }
+
+
+/**\fn main
+ *
+ * Standard application entry point
+ *
+ * @param argc (int) argument count
+ * @param argv (char**) array of char arrays representing the arguments
+ *
+ * @returns (int) return code for the application
+ */
+
+int main(int argc, char** argv){
+
+  ChannelDisplay* app = new ChannelDisplay;
+  char input;
+
+  std::cout << "\nReading MIDI input ... press <enter> to quit.\n";
+  std::cin.get(input);
+
+  delete app;
+
+  return 0;
+}
