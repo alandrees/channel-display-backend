dd71942726afc616094985f98059d3f8a62ea5a0
Alan Drees <alandrees@theselves.com>
2015-05-26 10:53:17 -0400

    optimized the includes to be more concise for the main function implementation

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,4 @@
 #include <cstdlib>
 #include <iostream>
 #include <string>
-#include <vector>
-#include <thread>
-#include "midi_input.hpp"
-#include "lcd_output.hpp"
 #include "channel_display.hpp"
-#include "callback.hpp"
-#include "wiringPi.h"
