ac25190105a9d3716e79da2084a2e95bae6f2859
Alan Drees <alandrees@theselves.com>
2015-05-26 10:53:56 -0400

    moved the main function from channel_display.cpp to main.cpp

--- src/channel_display.cpp
+++ src/channel_display.cpp
@@ -188,27 +188,3 @@ void ChannelDisplay::disableOutputs(){
 
   std::cout << this->output.size() << " outputs disabled\n";
 }
-
-
-/**\fn main
- *
- * Standard application entry point
- *
- * @param argc (int) argument count
- * @param argv (char**) array of char arrays representing the arguments
- *
- * @returns (int) return code for the application
- */
-
-int main(int argc, char** argv){
-
-  ChannelDisplay* app = new ChannelDisplay;
-  char input;
-
-  std::cout << "\nReading MIDI input ... press <enter> to quit.\n";
-  std::cin.get(input);
-
-  delete app;
-
-  return 0;
-}
--- src/main.cpp
+++ src/main.cpp
@@ -11,3 +11,26 @@
 #include <iostream>
 #include <string>
 #include "channel_display.hpp"
+
+/**\fn main
+ *
+ * Standard application entry point
+ *
+ * @param argc (int) argument count
+ * @param argv (char**) array of char arrays representing the arguments
+ *
+ * @returns (int) return code for the application
+ */
+
+int main(int argc, char** argv){
+
+  ChannelDisplay* app = new ChannelDisplay;
+  char input;
+
+  std::cout << "\nReading MIDI input ... press <enter> to quit.\n";
+  std::cin.get(input);
+
+  delete app;
+
+  return 0;
+}
